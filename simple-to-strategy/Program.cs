7ecd8fd76297d44cf414e6ee2d9f0420165b414c
unknown <sczarnecki@pgs052.pgs-soft.com>
2015-12-14 09:15:54 +0100

    Add sample to strategy refactor

---
+++ simple-to-strategy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace simple_to_strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Tax
+    {
+        public decimal Count(decimal netto, string countryCode)
+        {
+            if (countryCode == "PL")
+            {
+                return Decimal.Multiply(netto, 0.23m);
+            }
+            else if (countryCode == "DE")
+            {
+                return Decimal.Multiply(netto, 0.3m);
+            }
+            else if (countryCode == "EN")
+            {
+                return Decimal.Multiply(netto, 0.15m);
+            }
+
+            return 0;
+            
+            
+        }
+    }
+}
